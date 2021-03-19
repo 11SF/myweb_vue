8787146b8872ea97363e95c98fb00ccc967b7792
11SF <nsfolknsf@gmail.com>
2021-03-19 16:55:32 +0700

    add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,34 @@
 <template>
-  <v-app> 
-    <v-app-bar color="black" fixed dark>
-      <div class="container">
-        <div class="item">
-          <a href="/" id="logo">11SF</a>
-        </div>
-        <v-tabs align-with-title background-color="black">
-            <v-tab><router-link to="/" id="menu"> Page 1 </router-link></v-tab>
-            <v-tab><router-link to="/about" id="menu"> Page 2 </router-link></v-tab>
-        </v-tabs>
-        <v-spacer></v-spacer>
-        <div class="item">
-          <v-btn icon>
-            <v-icon>mdi-magnify</v-icon>
-          </v-btn>
-        </div>
-        <div class="item">
-          <v-btn icon>
-            <v-icon>mdi-heart</v-icon>
-          </v-btn>
-        </div>
-        <div class="item">
-          <v-btn icon>
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </div>
-      </div>
-    </v-app-bar>
-
-      <div class="wrap-content">
-        <router-view></router-view>
-      </div>
-
+  <v-app>
+    <Navbar />
+    <div class="wrap">
+      <router-view></router-view>
+    </div>
   </v-app>
 </template>
 
 <script>
+import Navbar from "./components/Navbar1";
 export default {
   name: "App",
+  components: {
+    Navbar,
+  },
 };
 </script>
-<style scoped>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,400;0,600;1,400&display=swap");
+* {
+  font-family: 'Kanit', sans-serif;
+}
 v-app {
   margin: 0;
   padding: 0;
   background-color: rgb(255, 255, 255);
 }
-.container {
-  display: flex;
-  padding: 0;
-}
-.item {
-  display: inline-block;
-  align-items: center;
-  margin: auto;
-}
-.group-menu {
-  margin-left: 20px;
-  display: flex;
-  align-items: center;
-}
-#logo {
-  text-decoration: none;
-  color: white;
-  font-size: 20px;
-}
-#menu {
-  text-decoration: none;
-  color: white;
-}
-.wrap-content {
-  margin-top: 50px;
+.wrap {
+  margin: 0 auto;
+  width: 100%;
+  padding-top: 3rem;
 }
 </style>
