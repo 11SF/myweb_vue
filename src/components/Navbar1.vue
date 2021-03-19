cfbad13bb372590bee97df7839119b32dbb2ee16
11SF <nsfolknsf@gmail.com>
2021-03-19 16:56:56 +0700

    init

---
+++ src/components/Navbar1.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <v-toolbar
+    color="black"
+    dark
+    >
+    
+        <v-toolbar-title>11SF</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+        <v-btn flat>Open jobs</v-btn>
+        <v-btn flat class="pink lighten-3">Sign In</v-btn>
+      </v-toolbar-items>
+    
+    </v-toolbar>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  transition: background-color 0.5s ease;
+  z-index: 1000;
+}
+</style>
