8a6b9bb0a842c1e5456f0657160fd8ae4a3a4c3f
11SF <nsfolknsf@gmail.com>
2021-03-24 04:07:50 +0700

    init

---
+++ src/components/Navbar2.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app-bar dark>
+    <v-app-bar-title>11SF Spotify Member</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-btn @click="goLogin()" v-if="isLogin === 'notlogin'"> Login</v-btn>
+    <v-btn @click="goLogout()" v-else> Logout </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Navbar2",
+  data() {
+    return {
+      isLogin: "",
+    };
+  },
+  methods: {
+    goLogin() {
+      this.$router.push({
+        name: "Login",
+      });
+    },
+    goLogout() {
+      localStorage.removeItem("userKey");
+      this.isLogin = 'notlogin'
+      this.$router.push({
+        name: "SpotifyMember",
+      });
+    },
+  },
+  created() {
+    let a = localStorage.getItem("userKey");
+    if (a != null) {
+      this.isLogin = "logedin";
+    } else {
+      this.isLogin = "notlogin";
+    }
+  },
+};
+</script>
+
+<style>
+</style>
