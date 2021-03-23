5986fda1d6d91de595e771c7780a5e480346a401
Nawapong Sitaruno <nawaphong.sit@dome.tu.ac.th>
2021-03-24 02:55:15 +0700

    init

---
+++ src/components/Navbar2.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app-bar color="primary" dark>
+        <v-app-bar-title>Spotify Member</v-app-bar-title>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: "Navbar2"
+}
+</script>
+
+<style>
+
+</style>
