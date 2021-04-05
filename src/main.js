f941f942beba62b12a273c0d31ae87ab0deddc6b
11SF <nsfolknsf@gmail.com>
2021-04-06 03:09:01 +0700

    add axios

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import vuetify from "./plugins/vuetify";
 import AOS from "aos";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "aos/dist/aos.css";
-
+import AxiosPlugin from 'vue-axios-cors';
+ 
+Vue.use(AxiosPlugin)
 Vue.config.productionTip = false;
 
 App.AOS = new AOS.init({ disable: "phone" });
